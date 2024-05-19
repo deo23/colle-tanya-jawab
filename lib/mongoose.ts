b48b7899dfe7e73aa23baee17d6c93b203b7f5dc
Muhammad Deo Audha Rizki <muhammad.deo.tif421@polban.ac.id>
2024-05-19 13:54:13 +0700

    chore: Update database name in mongoose.ts

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -12,7 +12,7 @@ export const connectToDatabase = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "DevOverflow",
+      dbName: "Colle-Tanya-Jawab",
     });
 
     isConnected = true;
